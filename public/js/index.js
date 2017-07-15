73b231487feabe60eb8829e39a15cffa8d2f060c
Cesar Rodas <rodascesar03@yahoo.com>
2017-07-14 20:06:27 -0400

    added icon and img files

--- public/js/index.js
+++ public/js/index.js
@@ -8,39 +8,39 @@ $(document).ready(function() {
   var cityName;
   var country;
   var init = true;
-  
-  $.getJSON('http://ipinfo.io', function(location){
+
+  $.getJSON('https://ipinfo.io', function(location){
     var loc = location.loc.split(",");
     lat = loc[0];
     lon = loc[1];
     cityName = location.city;
     country = location.country;
     //console.log(lat, lon, cityName, country );
-    
+
     img = new Image();
     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=13&size=400x400&sensor=false";
     $("#map").append(img);
-  
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=5ff25fbbab9159e9c404d8172c519d34", function(json){
-      console.log('json', json);    
+
+  $.getJSON("https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=5ff25fbbab9159e9c404d8172c519d34", function(json){
+      console.log('json', json);
       icon = new Image();
        //icon.addClass("icon");
-      icon.src = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png"; 
+      icon.src = "https://crossorigin.me/http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
       icon.setAttribute("class", "icon");
-          
+
       $("#location").append("<p class='info'>"+ cityName + ", " + country +"</p>");
 
       $("#description").append("<p class='info'>" + json.weather[0].main + "</p>");
       $("#description").append(icon);
-          
-      // This is the temperature 
+
+      // This is the temperature
       celcius = Math.floor(json.main.temp - 273.15);
       fahrenheit = Math.floor(((celcius * 9)/5) + 32);
       toggle();
 
-      // Sets up correct weather 
+      // Sets up correct weather
       var background = json.weather[0].description;
-          
+
       switch(background) {
         case "clear sky":
           $("body").removeClass();
@@ -87,7 +87,7 @@ $(document).ready(function() {
     });
   })
   .error(function() { alert("No location Available"); });
-  
+
   function toggle() {
     if(init){
       $("#temp").html( "<p class='text-center par' id='temp'>Temperature:</p>" +
@@ -97,7 +97,7 @@ $(document).ready(function() {
                        "<p class='info deg'>" + celcius + " &#8451; </p>");
     }
   }
-  
+
   $('#temp').on("click", function(){
     console.log("clock");
     init = !init;
